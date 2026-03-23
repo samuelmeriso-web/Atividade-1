e07d96ce037637fb48399c2e0e46dfb998bca2ea
samuelmeriso-web <samuel.meriso@escola.pr.gov.br>
2026-03-23 09:08:37 -0300

    Primeira atividade

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-// run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+// Multiplicação
+
+let num1 = 2;
+let num2 = 4;
+
+let soma = num1 + num2
+
+console.log("A soma é:",soma)
+
+// Subtração
+
+let subtracao = 50 - 25 ;
+
+console.log("Subtração é:",subtracao);
+
+// Divisão
+
+let divisao = 100 / 4 ;
+
+console.log("A divisao é:",divisao)
+
+
+
+
+
