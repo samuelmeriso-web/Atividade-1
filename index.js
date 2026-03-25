05053e52c19234f8d3c36bf2e06117a35bb0ce27
samuelmeriso-web <samuel.meriso@escola.pr.gov.br>
2026-03-25 09:09:25 -0300

    Atividade 1

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-// Multiplicação
+// Soma
 
 let num1 = 2;
 let num2 = 4;
 
-let soma = num1 + num2
+let soma = num1 + num2;
 
-console.log("A soma é:",soma)
+console.log("A soma é:",soma);
 
 // Subtração
 
@@ -16,10 +16,21 @@ console.log("Subtração é:",subtracao);
 
 // Divisão
 
-let divisao = 100 / 4 ;
+let divisao = 100 / 4;
 
-console.log("A divisao é:",divisao)
+console.log("A divisao é:",divisao);
 
+// Multiplicação
+
+let multipicacao = 50 * 30;
+
+console.log("O resultaddo multiplicação é:",multipicacao);
+
+// Igualdade
+
+let Igualdade = 10 == 10;
+
+console.log("A igualdade é:",Igualdade);
 
 
 
